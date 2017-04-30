2144eb3978b0ea2685054f8dffcd3451cd4a5eaf
Osvaldo Antonio Vega Aguero <osvaldo.vega@hpe.com>
2017-04-30 13:01:03 -0600

    Change rest base URL

--- src/js/services/config.js
+++ src/js/services/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   nasa: {
-    url: 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&',
-    key: '<here you put your NASA key>'
+    url: 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=',
+    key: '<Put here your NASA API Key>'
   }
 };

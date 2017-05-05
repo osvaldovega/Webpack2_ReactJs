3a4ddf0255f64c4cac2fb11ea84b87042a7f9d20
Osvaldo Antonio Vega Aguero <osvaldo.vega@hpe.com>
2017-05-05 15:09:59 -0600

    updated files

--- src/js/services/config.js
+++ src/js/services/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   nasa: {
     url: 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=',
-    key: '0LDuEGPf0Zbh1hVhIkPekDY6nOUy5aOmgKB6wAYS'
+    key: '<put here your NASA API key>'
   }
 };
